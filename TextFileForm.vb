0cd815374aba2a5467a86fbb46686aee2c3ba06a
Siamion Kutsen <kut@inp.bsu.by>
2026-02-24 12:30:52 +0300

    Add files via upload
    Version from 25 June 2009

---
+++ TextFileForm.vb
@@ -0,0 +1,8 @@
+﻿Public Class TextFileForm
+    Private Sub TextFileForm_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        'Dim RichTextBox1ReadOnly As Boolean
+        Me.Label1.Text = My.Resources.FileMsg & " " & strFileName
+        Me.RichTextBox1.Visible = True
+        Me.RichTextBox1.Text = strTextFile
+    End Sub
+End Class
